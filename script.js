956d14566551f0efcfd3ab13c3e3b1b2b0f070ea
gabrielluccini <gabriel.lucini@escola.pr.gov.br>
2025-09-08 11:05:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click" , MostraPalavrasChave);
+function MostraPalavrasChave(){
+    const texto = document.querySelector("#entrada-de-texto").value
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    campoResultado.textContent = texto;
+    const palavras = texto.split(" ");
+    campoResultado.textContent = palavras.join(", ");
+}
